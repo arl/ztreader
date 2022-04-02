25fc6b8f37b0d83a7d419f565ee1515d73a86e28
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-04-02 21:18:36 +0200

    Rework tests

--- reader_test.go
+++ reader_test.go
@@ -2,6 +2,7 @@ package zt
 
 import (
 	"bytes"
+	_ "embed"
 	"io"
 	"os"
 	"path/filepath"
@@ -9,45 +10,53 @@ import (
 	"testing"
 )
 
+var (
+	//go:embed testdata/lorem.txt.golden
+	lorem []byte
+
+	//go:embed testdata/lorem.txt.golden.gz
+	loremGzip []byte
+
+	//go:embed testdata/lorem.txt.golden.zst
+	loremZstd []byte
+)
+
+func testReader(r io.Reader) func(t *testing.T) {
+	return func(t *testing.T) {
+		r, err := NewReader(r)
+		if err != nil {
+			t.Fatalf("Reader returns %v", err)
+		}
+		got, err := io.ReadAll(r)
+		if err != nil {
+			t.Fatalf("couldn't read all: %v", err)
+		}
+
+		if !bytes.Equal(got, lorem) {
+			t.Errorf("read content doesn't match. Writing buffer to temp file")
+			tmpName := strings.ReplaceAll(t.Name(), string(filepath.Separator), "_") + "_"
+			f, err := os.CreateTemp("", tmpName)
+			if err != nil {
+				t.Fatalf("couldn't create temp file: %v", err)
+			}
+			if _, err := io.Copy(f, bytes.NewReader(got)); err != nil {
+				t.Fatalf("couldn't write to temp file: %v", err)
+			}
+			t.Logf("the content has been written to %q", f.Name())
+		}
+	}
+}
+
+func testReaders(newReader func([]byte) io.Reader) func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Run("uncompressed", testReader(newReader(lorem)))
+		t.Run("gzip", testReader(newReader(loremGzip)))
+		t.Run("zstd", testReader(newReader(loremZstd)))
+	}
+}
+
 func TestReader(t *testing.T) {
-	lorem := filepath.Join("testdata", "lorem.txt.golden")
-	want, err := os.ReadFile(lorem)
-	if err != nil {
-		t.Fatalf("can't read original file: %s", err)
-	}
-
-	fnames := []string{
-		lorem,
-		lorem + ".zst",
-		lorem + ".gz",
-	}
-
-	for _, fname := range fnames {
-		t.Run(fname, func(t *testing.T) {
-			f, err := os.Open(fname)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			r, err := NewReader(f)
-			if err != nil {
-				t.Fatalf("Reader returns %v", err)
-			}
-			got, err := io.ReadAll(r)
-			if err != nil {
-				t.Fatalf("couldn't read all: %v", err)
-			}
-
-			if !bytes.Equal(got, want) {
-				bad := filepath.Join(t.TempDir(), filepath.Base(fname))
-
-				t.Errorf("unexpected buffer content, writing it to: %q", bad)
-				if err := os.WriteFile(bad, got, 0644); err != nil {
-					t.Fatalf("can't write to %q, %v", bad, err)
-				}
-			}
-		})
-	}
+	t.Run("bytes", testReaders(func(buf []byte) io.Reader { return bytes.NewReader(buf) }))
 }
 
 func TestReader1Byte(t *testing.T) {
